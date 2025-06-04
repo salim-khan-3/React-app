4cdfba4a3cabf41e04419a7bcaf0b66813851286
Md salim islam <salimislam0036@gmail.com>
2025-06-04 17:17:15 +0600

    fixed some cleanup function problems

--- src/Components/hooks/useEffect/CleanUp.jsx
+++ src/Components/hooks/useEffect/CleanUp.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
+import "../../../style/cleanUp.css";
 export const CleanUp = () =>{
+    const[count,setCount] = useState(0);
+    useEffect(()=>{
+       const timer =  setInterval(()=>{
+            setCount((prev)=>prev+1)
+        },1000)
+
+        return () => clearInterval(timer);
+    },[])
     return( 
         <div className="CleanUp_container">
             <div className="sub_counter">
                 <p>My subscriber on my youtube.</p>
-                <div className="count"></div>
+                <div className="count">{count}</div>
                 <h3>Subscriber <br /> Realtime counter </h3>
             </div>
         </div>
