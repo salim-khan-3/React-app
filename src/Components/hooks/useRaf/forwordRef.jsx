f6b1204a60c2f81dd75227d78b5992682c30b21c
Md salim islam <salimislam0036@gmail.com>
2025-06-23 08:18:29 +0600

    forwardRef completed

--- src/Components/hooks/useRaf/forwordRef.jsx
+++ src/Components/hooks/useRaf/forwordRef.jsx
@@ -1,14 +1,15 @@
-import { useId, useRef } from "react";
+import { forwardRef, useId, useRef } from "react";
 import { UseRef } from "./useRaf"
 
 export const ForwardRefs = () =>{
 
-    const username = UseRef(null);
+    const username = useRef(null);
     const password = useRef(null);
 
     const handleFormSubmit = (e) =>{
         e.preventDefault();
         console.log(username.current.value);
+        console.log(password.current.value);
     }
     return (
         <form onSubmit={handleFormSubmit}>
@@ -19,11 +20,11 @@ export const ForwardRefs = () =>{
     )
 }
 
-const BeforeReact19Input = ((props,ref)=>{
+const BeforeReact19Input =forwardRef((Props,ref)=>{
     const id = useId();
     return(
         <div>
-            <label htmlFor={id}>{props.label}</label>
+            <label htmlFor={id}>{Props.label}</label>
             <input type="text" ref={ref}/>
         </div>
     )
